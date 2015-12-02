c40901a6719fb22172f19eafdba043f478e9c228
mjthomas <thomas.mikejohn@gmail.com>
2015-12-02 18:18:23 -0500

    added map

--- pset4_a.py
+++ pset4_a.py
@@ -1,6 +1,16 @@
+#Michael Thomas
+#BIOT-E 100
+#Assignment 4
+#zip-map
+
+#declare x,y,z
 x = ["a", "b", "c"]
 y = [1, 2, 3]
 z = ["m", "l"]
 
+#Zips x,y,z
 for i, j, k in zip(x, y, z):
     print i, j, k, "\n"
+
+#Use map to add "none" when the lists are un-even
+print map(None, x, y, z)
