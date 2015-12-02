99a43ed1b50a60ed387b79df147888650d9818c3
mjthomas <thomas.mikejohn@gmail.com>
2015-12-02 18:37:23 -0500

    add instruction codes

--- msa.py
+++ msa.py
@@ -2,3 +2,7 @@
 #BIOT-E 100
 #Assignment 4
 #MSA
+
+from Bio.SubsMat import MatrixInfo as myMatrices
+# and then
+matrix = myMatrices.blossum62
--- pickling.py
+++ pickling.py
@@ -2,3 +2,9 @@
 #BIOT-E 100
 #Assignment 4
 #pickling
+
+import cPickle as pickle
+saveFile = "neno.pkl"
+
+with open(saveFile, "wb") as output:
+    pickle.dump((d), output, protocol=pickle, HIGHEST_PROTOCOL)
