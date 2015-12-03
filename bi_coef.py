4416187b8ea6d3fc8a8230d795596d5347607f26
mjthomas <thomas.mikejohn@gmail.com>
2015-12-03 00:55:27 -0500

    with graph

--- bi_coef.py
+++ bi_coef.py
@@ -1,13 +1,28 @@
-   import math
-    x = int(input("Enter a value for x: "))
-    y = int(input("Enter a value for y: "))
-    if y == 1 or y == x:
-        print(1)
-    if y>x:
-        print(0)
+#Introduction to Bioinformatics Assignment 4
+#Purpose:Probability of Hamming Distance
+#Your Name: Michael Thomas
+#Date: 10/10/15
 
-    else:
-        a = math.factorial(x)
-        b = math.factorial(y)
-        div = (a//(b*(x-y)))
-        print(div)  
+import math
+import matplotlib.pyplot as plt
+
+a = int(input("Enter length of sequences: "))
+hamms=[]
+probs = []
+for i in xrange(0,int(a)):
+    hamms.append(i)
+
+for j in hamms:
+    bicoef = (math.factorial(a))/(math.factorial(j)*(math.factorial(a-j)))
+    perms = 4.0**a
+    prob = bicoef/perms
+    probs.append(prob)
+
+pmn = min(probs)
+pmx = max(probs)
+
+plt.scatter(hamms,probs)
+plt.axis([0,a,0,pmx + pmx*.25])
+plt.xlabel('hamming distance')
+plt.ylabel('probability')
+plt.show()
